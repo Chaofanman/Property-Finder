aa7832a29ae55a65187e822601dfd4fe085b7875
Chaofanman <gchase.patron@gmail.com>
2016-08-14 20:56:17 +0800

    Find out how to fix TyperError: Network request failed

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -7,19 +7,39 @@ import{
     TextInput,
     View,
     TouchableHighlight,
-    ActivityIndicatorIOS,
+    ActivityIndicator,
     Image
 } from 'react-native';
 import Style from './Style.js';
+
+function urlForQueryAndPage(key, value, pageNumber){
+    var data = {
+        country: 'uk',
+        pretty: '1',
+        encoding: 'json',
+        listing_type: 'buy',
+        action: 'search_listings',
+        page: pageNumber
+    };
+
+    data[key] = value;
+
+    var queryString = Object.keys(data).map(key => key + '=' + encodeURIComponent(data[key])).join('&');
+
+    return 'http://api.nestoria.co.uk/api?' + queryString;
+}
+
 
 class SearchPage extends Component{
     constructor(props){
         super(props);
         this.state = {
             searchString: 'London',
-            isLoading: false
+            isLoading: false,
+            message: ''
         };
         this.onSearchTermChange = this.onSearchTermChange.bind(this);
+        this.onSearchPressed = this.onSearchPressed.bind(this);
     }
 
     onSearchTermChange(event){
@@ -28,8 +48,43 @@ class SearchPage extends Component{
         console.log("current searchString: ", this.state.searchString);
     }
 
+    _executeQuery(query){
+        console.log("Query: ", query);
+        this.setState({ isLoading: true });
+        fetch(query)
+            .then((response) => response.json())
+            .then((json) => this._handleResponse(json.response))
+            .catch((error) => {
+                console.log("ERROR: ", error);
+                this.setState({
+                    isLoading: false,
+                    message: 'ERROR: ' + error
+                })
+            })
+    }
+
+    onSearchPressed(){
+        var query = urlForQueryAndPage('place_name', this.state.searchString, 1);
+        this._executeQuery(query);
+    }
+
+    _handleResponse(response){
+        this.setState({
+            isLoading: false,
+            message: ''
+        });
+        console.log("HANDLE_RESPONSE: ", response);
+        if (response.application_response_code(0, 1) === 1){
+            console.log("Properties found: " + response.listings.length);
+        } else {
+            this.setState({ message: 'Location not recognized' });
+        }
+    }
+
     
     render(){
+        var spinner = this.state.isLoading? (<ActivityIndicator size='large' />) : (<View />);
+
         return(
             <View style={Style.searchContainer}>
 
@@ -47,8 +102,11 @@ class SearchPage extends Component{
                         placeholder='Search via name or postal code' 
                         onChange={this.onSearchTermChange}
                         />
-                    <TouchableHighlight style={Style.button} underlayColor='#99d9f4'>
-                        <Text style={Style.buttonText}> Go </Text>
+                    <TouchableHighlight 
+                        style={Style.button} 
+                        underlayColor='#99d9f4'
+                        onPress={this.onSearchPressed}>
+                            <Text style={Style.buttonText}> Go </Text>
                     </TouchableHighlight>
                 </View>
 
@@ -57,6 +115,9 @@ class SearchPage extends Component{
                 </TouchableHighlight>
 
                 <Image source={require('../Resources/house.png')} style={Style.image}/>
+                {spinner}
+
+                <Text style={Style.description}>{this.state.message}</Text>
             </View>
         );
     }
